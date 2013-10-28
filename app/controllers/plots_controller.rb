a00e759098b500aaa86d7e1ae03e2514bd33da2f
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2013-10-28 14:09:19 +0100

    update

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -165,9 +165,69 @@ require 'paperclip'
      return R.resultsUnlist, R.theta0Values, R.theta1Values
   end
 
-  def multivariate_data
+  def multivariate_data(y, *args)
+
+       R.assign "Y", y
+       R.assign "X1", args[0]
+       R.assign "X2", args[1]
+       R.assign "X3", args[2] || nil if args[2].nil?
+
+       R.eval <<-EOF
+
+       alpha <- as.numeric(alpha)
+       num_iters <- as.numeric(num_iters)
+
+       x1 <- as.numeric(X1)
+       x2 <- as.numeric(X2)
+       x3 <- as.numeric(X3)
+        y <- as.numeric(Y)
+       numOfRows <- length(y)
 
 
+
+       if(length(x3) == 0) {
+          mX <- cbind(x1,x2)
+          mean <- cbind(mean(mX[,1]), mean(mX[,2]))
+          sd <- cbind(sd(mX[,1]), sd(mX[,2]))
+          theta = t(data.matrix(data.frame(theta0=0, theta1=0, theta2=0)))
+       } else {
+          mX <- cbind(x1,x2,x3)
+          mean <- cbind(mean(mX[,1]), mean(mX[,2]), mean(mX[,3]))
+          sd <- cbind(sd(mX[,1]), sd(mX[,2]), sd(mX[,3]))
+          theta = t(data.matrix(data.frame(theta0=0, theta1=0, theta2=0, theta3=0)))
+        }
+     
+       con <- rep(1, length(y))
+       mX <- (mX - (con %*% mean))/(con %*% sd)
+       cX <- cbind(x0=rep(1,each=length(y)), mX)
+
+      SEF <- function(cX, y, theta) {  
+             m <- length(y)
+             J <- 0         
+             J <- (1/(2*m)) * t((cX %*% theta) - y) %*% ((cX %*% theta) - y)  
+      }
+
+      multiGradDesc <- function(cX, y, theta, alpha, num_iters) {
+                       histSEF <- rep(0, each=num_iters)  
+                       m = length(y) 
+      
+           for(iter in 1:num_iters) {
+         
+                theta = theta - (alpha/m) * t(t((cX %*% theta) - y) %*% cX)
+                histSEF[iter] <- SEF(cX,y,theta)
+
+       }
+       output <- list(thetaOriVal=theta, J=histSEF)
+       return(output)
+      }
+
+   allResults <- multiGradDesc(cX,y,theta,alpha,num_iters)
+   resultsUnlist <- unlist(allResults) 
+
+   EOF
+       
+   return R.resultsUnlist
+      
   end
 
   def lm_method
