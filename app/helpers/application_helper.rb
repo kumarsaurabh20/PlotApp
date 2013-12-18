fa68bf33d35d9937596a75bd644e6541a558b671
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2013-12-18 21:32:50 +0100

    replicates implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,6 +27,11 @@ module ApplicationHelper
     image_tag("uaqua1_logo2.png", :alt => "S2C", :class => "round", :size => "100x75")
   end
 
+  def addrep
+    #image_tag("logo.png", :alt => "Sample App", :class => "round")
+    image_tag("add.png", :alt => "S2C", :class => "round", :size => "100x75")
+  end
+
 
  def betaLogo
      image_tag("beta.jpg", :alt => "beta_version", :class => "round", :size => "100x75")
