309f25223527f60a4a23048db022dbd285ba772a
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2014-02-03 11:06:36 +0100

    fixed untracked files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlotApp::Application.config.secret_token = '60bb4895a5016660b77e5a6633edcdba1390b2d8179782eea56ccdf33e4842da743463219515c9636111a0ba40cc8297d4f9d08ab6596dd5cd3711e761c50818'
---
+++ test/unit/helpers/plotting_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PlottingHelperTest < ActionView::TestCase
+end
