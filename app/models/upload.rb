166c0259b9743798486e85ab48fb87ddc03d61c9
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2014-04-02 00:30:50 +0200

    update

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,12 +2,15 @@ class Upload < ActiveRecord::Base
 
 attr_accessible :inten, :calib
 
-has_attached_file :inten
+has_attached_file :inten, :url => "/:attachment/:id/:style/:basename.:extension",
+                          :path => ":rails_root/public/:attachment/:basename.:extension"
 validates_attachment :inten, :presence => true,
   :content_type => { :content_type => ['text/csv', 'application/xls'] },
   :size => { :in => 0..50.megabytes }
 
-has_attached_file :calib
+
+has_attached_file :calib, :url => "/:attachment/:id/:style/:basename.:extension",
+                          :path => ":rails_root/public/:attachment/:basename.:extension"
 validates_attachment :calib, :presence => true,
   :content_type => { :content_type => ['text/csv', 'application/xls'] },
   :size => { :in => 0..50.megabytes }
