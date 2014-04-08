2bec1939ff51e882ea640147a5ee06c404d5d9aa
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2014-04-08 12:40:02 +0200

    update

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,8 +1,15 @@
 class UploadsController < ApplicationController
+
   # GET /uploads
   # GET /uploads.json
   def index
     @uploads = Upload.all
+    
+    @upload = Upload.new
+    
+    if @ipload
+    @show = Upload.find(params[:id])
+    end
 
     respond_to do |format|
       format.html # index.html.erb
@@ -10,32 +17,6 @@ class UploadsController < ApplicationController
     end
   end
 
-  # GET /uploads/1
-  # GET /uploads/1.json
-  def show
-    @upload = Upload.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @upload }
-    end
-  end
-
-  # GET /uploads/new
-  # GET /uploads/new.json
-  def new
-    @upload = Upload.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @upload }
-    end
-  end
-
-  # GET /uploads/1/edit
-  def edit
-    @upload = Upload.find(params[:id])
-  end
 
   # POST /uploads
   # POST /uploads.json
@@ -53,21 +34,7 @@ class UploadsController < ApplicationController
     end
   end
 
-  # PUT /uploads/1
-  # PUT /uploads/1.json
-  def update
-    @upload = Upload.find(params[:id])
-
-    respond_to do |format|
-      if @upload.update_attributes(params[:upload])
-        format.html { redirect_to @upload, notice: 'Upload was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @upload.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+ 
 
   # DELETE /uploads/1
   # DELETE /uploads/1.json
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,3 +1,17 @@
 class Upload < ActiveRecord::Base
-  # attr_accessible :title, :body
+# attr_accessible :title, :body
+
+has_attached_file :inten
+validates_attachment_presence :inten
+validates_attachment_size :inten, :less_than => 50.megabytes
+validates_attachment_content_type :inten, :content_type => ['text/csv', 'application/xls'],
+                                  :message => ', only text, csv and excel formats are allowed.'
+
+has_attached_file :calib
+validates_attachment_presence :calib
+validates_attachment_size :calib, :less_than => 50.megabytes
+validates_attachment_content_type :calib, :content_type => ['text/csv', 'application/xls'],
+                                  :message => ', only text, csv and excel formats are allowed.'
+
+
 end
